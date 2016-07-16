c56a12cecf6a2f1ab608efd1391b230b7b9ecb62
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-16 19:12:50 +0300

    Added nodemon & notes for routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.json();
 });
-
+router.get('/politician/:pid', function(req, res, next) {
+	// req.params.pid
+  res.json()
+});
 module.exports = router;
