9e8d4323397aad6949f0329b327cd0cf4b926863
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-17 14:44:37 +0300

    Added route for retrieving a states politicians

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var openSecrets = require('../public/seedData/openSecrets')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   res.json();
 });
+
 router.get('/politician/:pid', function(req, res, next) {
-	// req.params.pid
+    // req.params.pid
   res.json()
+});
+
+router.get('/:sid/politicians', function(req, res, next) {
+    openSecrets.getStateLegistators(req.params.sid, function (data) {
+        res.json(data)
+    });
 });
 module.exports = router;
