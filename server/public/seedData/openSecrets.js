ba941f8ed349275a6b338d103f569d0423ae502b
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-17 16:51:12 +0300

    Fixed Bug: Handles responses from Open Secrets for states with only one legislator

--- server/public/seedData/openSecrets.js
+++ server/public/seedData/openSecrets.js
@@ -2,6 +2,32 @@ var request = require('request');
 var states = require('./statesList');
 var apiKey = require('../../apikeys').openSecrets;
 
+
+parseLegislators = function (body) {
+	parseLegislator = function (legislator) {
+		legislator = legislator['@attributes'];
+		return {
+			openSecretsID: legislator.cid, 
+			name: legislator.firstlast, 
+			gender: legislator.gender, 
+			party: legislator.party, 
+			website: legislator.website,
+			twitter: legislator.twitter_id,
+			youtube: legislator.youtube_url,
+			form: legislator.webform,
+			birthday: legislator.birthday
+		};
+	}
+	
+	var legislators = JSON.parse(body).response.legislator;
+	var multipleLegislators = Array.isArray(legislators);
+
+	return multipleLegislators ? legislators.map(function (legislator) {
+		return parseLegislator(legislator);
+	}) : parseLegislator(legislators);
+
+}
+
 exports.getStateLegistators = function (stateID, send) {
 	var stateID = stateID || 0;
 	var state = states[stateID].abbreviation;
@@ -11,20 +37,7 @@ exports.getStateLegistators = function (stateID, send) {
 			console.log(err)
 		}
 		else {
-			send(JSON.parse(body).response.legislator.map(function (legislator) {
-				legislator = legislator['@attributes'];
-				return {
-					openSecretsID: legislator.cid, 
-					name: legislator.firstlast, 
-					gender: legislator.gender, 
-					party: legislator.party, 
-					website: legislator.website,
-					twitter: legislator.twitter_id,
-					youtube: legislator.youtube_url,
-					form: legislator.webform,
-					birthday: legislator.birthday
-				};
-			}));
+			send(parseLegislators(body));
 		}
 
 	});
