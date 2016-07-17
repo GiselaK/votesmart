cf7d02e9e551d717479e827c5ab11b3fe3c4390d
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-17 14:55:36 +0300

    Does a git to Open Secrets API for a states legislaturs, parses the data recieved, sends it back to the client as json. Added json file of states names and abbreviations so when a state is picked we can find the abbreviation and use it for the Open Secrets git request

---
+++ server/public/seedData/openSecrets.js
@@ -0,0 +1,32 @@
+var request = require('request');
+var states = require('./statesList');
+var apiKey = require('../../apikeys').openSecrets;
+
+exports.getStateLegistators = function (stateID, send) {
+	var stateID = stateID || 0;
+	var state = states[stateID].abbreviation;
+	var legislators = [];
+	console.log(state)
+	request("http://www.opensecrets.org/api/?method=getLegislators&id=" + state + "&apikey=" + apiKey + "&output=json", function (err, resp, body) {
+		if (err) {
+			console.log(err)
+		}
+		else {
+			send(JSON.parse(body).response.legislator.map(function (legislator) {
+				legislator = legislator['@attributes'];
+				return {
+					openSecretsID: legislator.cid, 
+					name: legislator.firstlast, 
+					gender: legislator.gender, 
+					party: legislator.party, 
+					website: legislator.website,
+					twitter: legislator.twitter_id,
+					youtube: legislator.youtube_url,
+					form: legislator.webform,
+					birthday: legislator.birthday
+				};
+			}));
+		}
+
+	});
+}
