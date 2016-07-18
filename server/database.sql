1133ec0aceb257620d762ee058906717acca492c
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-18 11:47:20 +0300

    Legislators Table SQL written

---
+++ server/database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Legislators (
+	ID INTEGER PRIMARY KEY,
+	openSecretsID varchar(255),
+	sunlightid varchar(255),
+	birthday varchar(255),
+	gender varchar(255),
+	img varchar(255),
+	party varchar(255),
+	phone varchar(255),
+	state varchar(255),
+	twitter varchar(255),
+	webform varchar(255),
+	website varchar(255),
+	youtube varchar(255)
+)
+
