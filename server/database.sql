805f778c7b70cf1b256802579cedfa4835c1a0e3
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-19 09:11:18 +0300

    Added parsing stated bills logic & a table for bills

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS Legislators (
-	ID INTEGER PRIMARY KEY,
+	ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	openSecretsID varchar(255),
 	sunlightid varchar(255),
 	birthday varchar(255),
@@ -13,5 +13,16 @@ CREATE TABLE IF NOT EXISTS Legislators (
 	webform varchar(255),
 	website varchar(255),
 	youtube varchar(255)
+)
+
+CREATE TABLE IF NOT EXISTS Bills (
+	ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	sunlightid varchar(255),
+	created_at varchar(255),
+	updated_at varchar(255),
+	chamber varchar(255),
+	state varchar(255),
+	session varchar(255),
+	bill_id varchar(255)
 )
 
--- server/public/seedData/sunlightlabs.js
+++ server/public/seedData/sunlightlabs.js
@@ -1,20 +1,45 @@
 var request = require('request');
 var statesList = require('./statesList');
 var apiKey = require('../../apikeys').sunlightlabs;
+var Promise = require("bluebird");
 
-exports.seedBills = function () {
+exports.seedBills = function (send) {
   // statesList.forEach(function (state) {
     var page = 1;
     var nextPage = true;
-    while (nextPage) {
-    console.log("whats taking so long")
-      request("http://openstates.org/api/v1/bills/?state=" + "ca" + "&apikey=" + apiKey + "&sort=created_at&page=" + page, function (err, resp, body) {
-        console.log(body)
-      })
-      nextPage = false;
+    var data = [];
+    var startTime = Date.now();
 
-      page++;
+    var getStateBills = function (state, page) {
+      page = page || 1;
+      request("http://openstates.org/api/v1/bills/?state=" + state + "&apikey=" + apiKey + "&sort=created_at&page=" + page, function (err, resp, body) {
+        if (body) {
+          data.push(body);
+        }
+        console.log("body:", body)
+        if (body) {
+          if (typeof JSON.parse(data[data.length-1])[0] === "object") {
+
+            // try {
+            //   var blah = JSON.parse(data[data.length-1])[0].title
+              
+            // } catch (e) {
+              console.log(JSON.parse(data[data.length-1])[0]);
+            // }
+            
+          } 
+          // getStateBills(state, page + 1)
+        }
+
+      })
+      
     }
-  // })
+    var getAllStatesBills = function (cb) {
+      statesList.forEach(function (state) {
+        getStateBills(state.abbreviation)
+      })
+    }
+    getAllStatesBills();
+
 }
 //TODO: Find a way to store each pages contents from SunlightLabs API
