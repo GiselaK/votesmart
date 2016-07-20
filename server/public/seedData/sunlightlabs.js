24d4788456a72d9a7e9d65477d5f7c969fd9290d
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-20 16:57:58 +0300

    Now only does get requests for bills that have been updated since the last get request

--- server/public/seedData/sunlightlabs.js
+++ server/public/seedData/sunlightlabs.js
@@ -2,30 +2,57 @@ var request = require('request');
 var statesList = require('./statesList');
 var apiKey = require('../../apikeys').sunlightlabs;
 var bills = require('../../dbOperations/bills')
+var sunlightLog = require('../../dbOperations/sunlightLog')
+var Promise = require("bluebird");
+
 
 exports.seedBills = function (send) {
     var page = 1;
-    
-    var getStateBills = function (state, page) {
+
+
+    var getStateBills = function (state, date, page) {
       page = page || 1;
-   
-      request("http://openstates.org/api/v1/bills/?state=" + state + "&apikey=" + apiKey + "&sort=created_at&page=" + page, function (err, resp, body) {
+
+      var apiReq = "http://openstates.org/api/v1/bills/?state=" + state + 
+      "&apikey=" + apiKey + "&sort=created_at&page=" + page;
+      
+      if (date) {
+        console.log("added date to req")
+        apiReq+="&updated_since=" + date;
+        console.log(apiReq)
+      } else {
+        console.log("ERROR: Sunlight Labs API getStateBills")
+      }
+
+      request(apiReq, function (err, resp, body) {
+           // console.log("body:", body)
         if (body) {
             bills.addBills(JSON.parse(body))
-            getStateBills(state, page + 1)
+            // getStateBills(state, date, page + 1)
         }
 
       })
       
     }
 
-    var getAllStatesBills = function (cb) {
-      statesList.forEach(function (state) {
-        getStateBills(state.abbreviation)
-      })
-
+    var getAllStatesBills = function (date) {
+      // statesList.forEach(function (state) {
+        // getStateBills(state.abbreviation)
+      // })
+      console.log("yay")
+      getStateBills("VA", date)
     }
     
-    getAllStatesBills();
+    var getLastLogDate = function () {
+        return new Promise(function (resolve, reject) {
+          sunlightLog.retrieveLastLogDate(function(date) {
+            resolve(date)
+          })
+        }).then(function (date) {
+          getAllStatesBills(date);
+        })
+    }
+    getLastLogDate();
+    sunlightLog.addDateToLog();
 
 }
