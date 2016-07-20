69f4fafca1a4839ffdd5d34ca71b0f62a5bbec09
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-20 15:16:30 +0300

    Added spacing for clarity

--- server/public/seedData/sunlightlabs.js
+++ server/public/seedData/sunlightlabs.js
@@ -1,15 +1,14 @@
 var request = require('request');
 var statesList = require('./statesList');
 var apiKey = require('../../apikeys').sunlightlabs;
-var Promise = require("bluebird");
 var bills = require('../../dbOperations/bills')
 
 exports.seedBills = function (send) {
-  // statesList.forEach(function (state) {
     var page = 1;
-
+    
     var getStateBills = function (state, page) {
       page = page || 1;
+   
       request("http://openstates.org/api/v1/bills/?state=" + state + "&apikey=" + apiKey + "&sort=created_at&page=" + page, function (err, resp, body) {
         if (body) {
             bills.addBills(JSON.parse(body))
@@ -19,12 +18,14 @@ exports.seedBills = function (send) {
       })
       
     }
+
     var getAllStatesBills = function (cb) {
       statesList.forEach(function (state) {
         getStateBills(state.abbreviation)
       })
+
     }
+    
     getAllStatesBills();
 
 }
-//TODO: Find a way to store each pages contents from SunlightLabs API
