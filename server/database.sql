f1405327f405579447ef03e1e1f4be63253eaf80
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-24 11:35:10 +0300

    Updated varchar sizes

--- server/database.sql
+++ server/database.sql
@@ -4,11 +4,12 @@ CREATE TABLE IF NOT EXISTS Legislators (
 	openSecretsID varchar(255),
 	sunlightid varchar(255),
 	birthday varchar(255),
-	gender varchar(255),
+	gender varchar(1),
 	img varchar(255),
+	name varchar (255),
 	party varchar(255),
 	phone varchar(255),
-	state varchar(255),
+	state varchar(2),
 	twitter varchar(255),
 	webform varchar(255),
 	website varchar(255),
@@ -17,12 +18,12 @@ CREATE TABLE IF NOT EXISTS Legislators (
 
 CREATE TABLE IF NOT EXISTS Bills (
 	ID SERIAL NOT NULL PRIMARY KEY,
-	title varchar(1000),
+	title varchar(2000),
 	sunlightid varchar(255),
 	created_at varchar(255),
 	updated_at varchar(255),
 	chamber varchar(255),
-	state varchar(255),
+	state varchar(2),
 	session varchar(255),
 	bill_id varchar(255)
 );
