f28ed591ce6bede90f8d230407eaf600fba9372b
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2016-07-24 09:48:05 +0300

    Added base url var instead of hardcoding it into the request and began building getBillInfo request functionality

--- server/public/seedData/sunlightlabs.js
+++ server/public/seedData/sunlightlabs.js
@@ -5,54 +5,59 @@ var bills = require('../../dbOperations/bills')
 var sunlightLog = require('../../dbOperations/sunlightLog')
 var Promise = require("bluebird");
 
+var baseURL = "http://openstates.org/api/v1/bills/";
 
 exports.seedBills = function (send) {
     var page = 1;
 
+    var getBillVoteInfo = function (state, session, bill_id) {
+      var apiReq = baseURL + "/" + state + "/" + session + "/" + bill_id + "?apikey=" + apiKey;
+      request(apiReq, function (err, resp, body) {
+        bills.addBillDetails(JSON.parse(body));
+      })
+    }
 
-    var getStateBills = function (state, date, page) {
+    var getStateBills = function (state, lastUpdateDate, page) {
       page = page || 1;
 
-      var apiReq = "http://openstates.org/api/v1/bills/?state=" + state + 
+      var apiReq = baseURL + "?state=" + state + 
       "&apikey=" + apiKey + "&sort=created_at&page=" + page;
-      
-      if (date) {
-        console.log("added date to req")
-        apiReq+="&updated_since=" + date;
-        console.log(apiReq)
+
+      if (lastUpdateDate) {
+        apiReq+="&updated_since=" + lastUpdateDate;
       } else {
-        console.log("ERROR: Sunlight Labs API getStateBills")
+        console.log("ERROR: Sunlight Labs API getStateBills. If the db is seeded this code should not be ran (This is regarding a problem with the SunLightLog table) If this is the first time seeding the database, it should reach here but this process will take hours. Please close the server to avoid this insanely long seeding process")
       }
 
       request(apiReq, function (err, resp, body) {
-           // console.log("body:", body)
         if (body) {
             bills.addBills(JSON.parse(body))
-            // getStateBills(state, date, page + 1)
+            getStateBills(state, lastUpdateDate, page + 1)
         }
 
       })
       
-    }
+    };
 
-    var getAllStatesBills = function (date) {
-      // statesList.forEach(function (state) {
-        // getStateBills(state.abbreviation)
-      // })
-      console.log("yay")
-      getStateBills("VA", date)
+
+    var getAllStatesBills = function (lastUpdateDate) {
+      statesList.forEach(function (state) {
+        getStateBills(state.abbreviation, lastUpdateDate)
+      })
     }
     
     var getLastLogDate = function () {
         return new Promise(function (resolve, reject) {
-          sunlightLog.retrieveLastLogDate(function(date) {
-            resolve(date)
+          sunlightLog.retrieveLastLogDate(function(lastUpdateDate) {
+            resolve(lastUpdateDate)
           })
-        }).then(function (date) {
-          getAllStatesBills(date);
+        }).then(function (lastUpdateDate) {
+          getAllStatesBills(lastUpdateDate);
         })
     }
-    getLastLogDate();
-    sunlightLog.addDateToLog();
+
+    // getLastLogDate();
+    getBillVoteInfo("ny", "2015-2016", "S 6545")
+   
 
 }
